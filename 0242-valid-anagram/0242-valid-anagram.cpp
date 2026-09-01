8ea02ee9694f2e5872e873a11d004df5bf644d3f
iharshilgupta <gupharshil007@gmail.com>
2026-09-01 08:17:55 +0530

    Time: 0 ms (100%), Space: 10 MB (11.43%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false; // edge cases
+        unordered_map<char,int> freq1;
+        unordered_map<char,int> freq2;
+        for(char c:s){
+            freq1[c]++;
+        }
+        for(char c:t){
+            freq2[c]++;
+        }
+       return freq1==freq2; // new concept
+    }
+};
