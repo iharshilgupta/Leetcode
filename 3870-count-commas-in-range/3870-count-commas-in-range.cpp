91dabb6cd73356bc3dc7bbb59a4b3bd02cb03300
iharshilgupta <gupharshil007@gmail.com>
2026-09-09 07:42:56 +0530

    Time: 0 ms (100%), Space: 8.4 MB (84.7%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int comma=1;
-        int total=0;
+        // int comma=1;
+        // int total=0;
+        // if(n<=999){
+        //     return 0;
+        // }
+        // for(int i=1000;i<=n;++i){
+        //     total+=comma;
+        // }
+        // return total;
         if(n<=999){
             return 0;
         }
-        for(int i=1000;i<=n;++i){
-            total+=comma;
+        else{
+            return n-1000+1;
         }
-        return total;
     }
 };
