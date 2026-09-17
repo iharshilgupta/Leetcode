0d01a335206cccd2fcec0e15a2335e8ca58288c6
iharshilgupta <gupharshil007@gmail.com>
2026-09-17 22:35:23 +0530

    Time: 23 ms (2.1%), Space: 14.9 MB (7.21%) - LeetHub

---
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int smallestNumber(int n) {
+        int x=n;
+        while(true){
+            std::bitset<16> bin(x);
+            string s=bin.to_string();
+            size_t one=s.find('1');
+            std::string valid=(one==string::npos) ?"0":s.substr(one);
+            bool l=true;
+            for(char c:valid){
+                if(c!='1'){
+                    l=false;
+                    break;
+                }
+            }
+            if(l) return x;
+            x++;
+        }
+        
+
+        // int bin=32-__builtin_clz(n);
+        // return(1<<bin)-1;
+    }
+};
